0eefa2e2004006dd2c7385e81ec8de85d4391db4
Johan Wejdenstolpe <johan.wejdenstolpe@bredband.net>
2023-02-14 09:43:04 +0100

    Add-project

---
+++ PikitiaUI/PikitiaUI/PikitiaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PikitiaUIApp.swift
+//  PikitiaUI
+//
+//  Created by Johan Wejdenstolpe on 2023-02-14.
+//
+
+import SwiftUI
+
+@main
+struct PikitiaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
