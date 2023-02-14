b9723c75d5c5c1c7c84547b7c137c6222ac12ba2
Johan Wejdenstolpe <johan.wejdenstolpe@bredband.net>
2023-02-14 09:55:03 +0100

    Add-flickr-api-and-create-models

---
+++ PikitiaUI/PikitiaUI/Models/Photo.swift
@@ -0,0 +1,32 @@
+//
+//  Photo.swift
+//  Pikitia
+//
+//  Created by Johan Wejdenstolpe on 2023-02-11.
+//
+
+import Foundation
+
+struct Photo: Codable, Identifiable, Hashable {
+    func hash(into hasher: inout Hasher) {
+      hasher.combine(id)
+    }
+
+    static func == (lhs: Photo, rhs: Photo) -> Bool {
+      lhs.id == rhs.id
+    }
+    
+    let id: String
+    let owner: String
+    let secret: String
+    let server: String
+    let farm: Int
+    let title: String
+    let ispublic: Int
+    let isfriend: Int
+    let isfamily: Int
+    
+    var imageURLString: String {
+        return "https://live.staticflickr.com/\(server)/\(id)_\(secret).jpg"
+    }
+}
---
+++ PikitiaUI/PikitiaUI/Models/PhotosData.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosData.swift
+//  Pikitia
+//
+//  Created by Johan Wejdenstolpe on 2023-02-11.
+//
+
+import Foundation
+
+struct PhotosData: Codable {
+    let photos: Photos
+}
